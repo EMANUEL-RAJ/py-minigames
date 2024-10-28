3ec27fbae43d4639a6e9388835b8e6161a4b125f
Emanuel Raj <emanuelraj.1999@gmail.com>
2024-10-28 20:01:09 +0530

    snake-basic: Blitting score to window

--- minigames/snake/main.py
+++ minigames/snake/main.py
@@ -6,11 +6,17 @@ from config import *
 
 # --- Game Variables ---
 running = True
+score = 0
 
 # --- Initializing Pygame and Setting Clock---
 pygame.init()
 WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
+
+# --- Building In-Game Assets ---
+font = pygame.font.SysFont("freesansbold", 15)
+score_txt = font.render("Score: " + str(score), True, SCORE_COLOR)
+score_rect = score_txt.get_rect(topleft=SCORE_RECT)
 
 while running:
     for event in pygame.event.get():
@@ -18,6 +24,7 @@ while running:
             running = False
 
     WIN.fill(BACKGROUND_COLOR)
+    WIN.blit(score_txt, score_rect)
     pygame.display.update()
     clock.tick(FPS)
 
