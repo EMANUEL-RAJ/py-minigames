91951c165855701e007c2b4394cb40ca4a90e332
Emanuel Raj <emanuelraj.1999@gmail.com>
2025-10-27 17:44:37 +0530

    Adding paddle to pong game

--- src/games/pong.py
+++ src/games/pong.py
@@ -8,15 +8,16 @@ Includes structured logging with Rich.
 import sys
 
 import pygame
-from src.logger.game_logger import logger
-
+from libs.logger.game_logger import logger
 
 # ---------------------------------------------------------------------------
 # Configuration Constants
 # ---------------------------------------------------------------------------
 GAME_TITLE = "Pong"
-WINDOW_SIZE = (500, 500)
+WINDOW_WIDTH = 500
+WINDOW_HEIGHT = 500
 BACKGROUND_COLOR = (0, 0, 0)
+WHITE = (255, 255, 255)
 FPS = 60
 
 
@@ -31,11 +32,12 @@ class PongGame:
         pygame.init()
         pygame.display.set_caption(GAME_TITLE)
 
-        self.display_surface = pygame.display.set_mode(WINDOW_SIZE)
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.clock = pygame.time.Clock()
         self.is_running = True
+        self.player_paddle = Paddle(self.display_surface)
 
-        logger.info("PongGame initialized with window size %s", WINDOW_SIZE)
+        logger.info("PongGame initialized with window size %s", (WINDOW_WIDTH, WINDOW_HEIGHT))
 
     # -----------------------------------------------------------------------
     # Main Loop
@@ -48,7 +50,6 @@ class PongGame:
             self._handle_events()
             self._update()
             self._render()
-
             self.clock.tick(FPS)
 
         self._shutdown()
@@ -62,6 +63,11 @@ class PongGame:
             logger.debug("Event detected: %s", event)
             if event.type == pygame.QUIT:
                 self.is_running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.player_paddle.move_left()
+        elif keys[pygame.K_RIGHT]:
+            self.player_paddle.move_right()
 
     # -----------------------------------------------------------------------
     # Game Logic
@@ -76,6 +82,7 @@ class PongGame:
     def _render(self) -> None:
         """Draw all visual elements on the screen."""
         self.display_surface.fill(BACKGROUND_COLOR)
+        self.player_paddle.draw()
         pygame.display.flip()
 
     # -----------------------------------------------------------------------
@@ -86,6 +93,32 @@ class PongGame:
         pygame.quit()
         logger.info("Game shutdown complete")
         sys.exit()
+
+
+class Paddle:
+    """Paddle class responsible for initializing and controlling paddle"""
+
+    def __init__(self, display):
+        self.display = display
+        self.x_pos = 250
+        self.y_pos = 250
+        self.width = 50
+        self.height = 5
+        self.speed = 10
+        self.rect = pygame.Rect((self.x_pos, self.y_pos), (self.width, self.height))
+
+    def draw(self):
+        pygame.draw.rect(self.display, WHITE, self.rect)
+
+    def move_left(self):
+        self.rect.left -= self.speed
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+    def move_right(self):
+        self.rect.right += self.speed
+        if self.rect.right > WINDOW_WIDTH:
+            self.rect.right = WINDOW_WIDTH
 
 
 # ---------------------------------------------------------------------------
