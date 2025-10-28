40dffd72445725b060fa5c5f71c1fed79310dd96
Emanuel Raj <emanuelraj.1999@gmail.com>
2025-10-29 00:49:14 +0530

    ping pong: Created game screen

---
+++ src/games/pingpong/main.py
@@ -0,0 +1,54 @@
+import sys
+
+import pygame
+
+from .constants import *
+from libs.logger.game_logger import logger
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(GAME_TITLE)
+        self.running = True
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT))
+
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                logger.info("Game terminated by user.")
+
+    def _update(self):
+        pygame.display.update()
+
+    def _render(self):
+        self.screen.fill(GAME_BG_CLR)
+        pygame.draw.line(self.screen, GAME_OBJ_CLR, SCREEN_DIV[0], SCREEN_DIV[1], 1)
+        self.clock.tick(GAME_FPS)
+
+    def run(self):
+        """
+        Runs main game loop
+        :return: None
+        """
+        while self.running:
+            self._handle_events()
+            self._update()
+            self._render()
+
+        pygame.quit()
+        sys.exit(0)
+
+
+def main() -> None:
+    try:
+        game = Game()
+        game.run()
+    except Exception as exc:
+        logger.exception("Unhandled execption: %s", exc)
+
+
+if __name__ == "__main__":
+    main()
